a825dff7e0fa041fc2a6a3f52d27ac1726e420f6
jeanneps <jeanepacchecosoares@gmail.com>
2024-10-01 00:59:05 -0300

    feito site simples usando html, css e javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const switcher = document.querySelector('.botao');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme');
+
+    let className = document.body.className;
+    if(className === "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
